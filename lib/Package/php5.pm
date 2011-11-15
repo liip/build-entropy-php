a84738d1aae65526cf3d825ebe818afe74745545
Christian Stocker <me@chregu.tv>
2011-11-15 11:51:32 +0100

    added --enable-zend-multibyte

--- lib/Package/php5.pm
+++ lib/Package/php5.pm
@@ -101,7 +101,8 @@ sub configure_flags {
 		'--with-mysql=mysqlnd',
 		'--with-mysqli=mysqlnd',
 		'--with-pdo-mysql=mysqlnd',
-		'--enable-pcntl'
+		'--enable-pcntl',
+		'--enable-zend-multibyte'
 	);
 
 	push @extension_flags, $self->dependency_extension_flags(%args);
