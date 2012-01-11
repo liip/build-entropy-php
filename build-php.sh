d9d81154affa86da10b027575bd7e3ec720873ab
Christian Stocker <me@chregu.tv>
2012-01-11 17:57:48 +0100

    take standard perl

--- build-php.sh
+++ build-php.sh
@@ -3,4 +3,4 @@
 export PHP_AUTOCONF=$PWD/extras/autoconf2.13/bin/autoconf 
 export PHP_AUTOHEADER=$PWD/extras/autoconf2.13/bin/autoheader 
 rm -rf /tmp/build-entropy-php-pkg/php5/ /tmp/build-entropy-php-pkgdst/entropy-php.pkg
-nice -n 19 perl5.10.0 -Ilib build-php.pl
+nice -n 19 perl -Ilib build-php.pl
