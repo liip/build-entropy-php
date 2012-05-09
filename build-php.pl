1ca863fc914176e664c68018ab3a43deaf71327c
Christian Stocker <me@chregu.tv>
2012-05-09 17:06:51 +0200

    PHP 5.3.13

--- build-php.pl
+++ build-php.pl
@@ -50,7 +50,7 @@ my $config = Config->new(
 			suffix       => '-apache2',
 		},
 	},
-	version              => '5.3.12',
+	version              => '5.3.13',
 	release              => 1,
 	debug                => 1,
 );
