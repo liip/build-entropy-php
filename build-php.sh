7b7623bb1c5729815738faf68f7806cbb7ce02b9
Christian Stocker <me@chregu.tv>
2013-03-17 22:04:56 +0100

    show git diff at the end for making sure we commit everything

--- build-php.sh
+++ build-php.sh
@@ -4,3 +4,5 @@ export PHP_AUTOCONF=$PWD/extras/autoconf2.13/bin/autoconf
 export PHP_AUTOHEADER=$PWD/extras/autoconf2.13/bin/autoheader 
 rm -rf /tmp/build-entropy-php-pkg/php5/ /tmp/build-entropy-php-pkgdst/entropy-php.pkg
 nice -n 19 perl -Ilib build-php.pl
+
+git diff
