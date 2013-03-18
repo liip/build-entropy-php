f4ddf3b05831baee3b076a6ab11641ba634d0c08
Christian Stocker <me@chregu.tv>
2013-03-18 09:56:23 +0100

    only run git diff, when perl script succeeded

--- build-php.sh
+++ build-php.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 rm -rf /tmp/build-entropy-php-pkg/php5/ /tmp/build-entropy-php-pkgdst/entropy-php.pkg
-nice -n 19 perl -Ilib build-php.pl
-
-git diff
+nice -n 19 perl -Ilib build-php.pl && git diff
