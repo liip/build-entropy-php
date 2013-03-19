1b89373475e8514e8713da1bda8d68a4c0bd26aa
Christian Stocker <me@chregu.tv>
2013-03-19 07:27:49 +0100

    sync libmemcached to 5_3_mountainlion

--- lib/Package/libmemcached.pm
+++ lib/Package/libmemcached.pm
@@ -11,7 +11,9 @@ sub dependency_names {
         return qw(libevent);
 }
 sub base_url {
-	return "https://launchpad.net/libmemcached/1.0/$VERSION/+download/";
+#	return "http://download.tangent.org/";
+return "https://launchpad.net/libmemcached/1.0/$VERSION/+download/";
+
 }
 
 sub packagename {
@@ -28,12 +30,12 @@ sub filename {
 
 sub is_built {
 	my $self = shift @_;
-	return -e $self->packagesrcdir() . "/libmemcached.4.dylib";
+	return -e $self->packagesrcdir() . "/libmemcached.dylib";
 }
 
 sub subpath_for_check {
 	my $self = shift @_;
-	return "lib/libmemcached.4.dylib";
+	return "lib/libmemcached.dylib";
 }
 
 sub configure_flags {
