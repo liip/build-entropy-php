c4c76a51250e5921b4b3cc839f7360fef41aa1f5
Patrick Jezek <patrick@jezek.ch>
2013-06-15 02:40:15 +0200

    Added libssh2 and pecl/ssh2 extension.

---
+++ lib/Package/ssh2.pm
@@ -0,0 +1,42 @@
+package Package::ssh2;
+
+use strict;
+use warnings;
+
+use base qw(Package::peclbase);
+
+our $VERSION = '0.12';
+
+sub init {
+        my $self = shift;
+        $self->SUPER::init(@_);
+        $self->{PACKAGE_NAME} = 'ssh2';
+        $self->{VERSION} = $VERSION;
+}
+
+
+sub base_url {
+    return "http://pecl.php.net/get";
+}
+
+sub packagesrcdir {
+        my $self = shift @_;
+        return $self->config()->srcdir() . "/" .  $self->packagename() . "/" ;
+}
+
+sub configure_flags {
+	my $self = shift @_;
+	return join " ", (
+		$self->SUPER::configure_flags(@_),
+		'--enable-' . lc($self->{PACKAGE_NAME}),
+		'--with-php-config=' . $self->install_prefix() . '/bin/php-config',
+        '--with-ssh2=/usr/local/php5'
+	);
+}
+
+sub extension_ini{
+    my ($self, $dst) = @_;
+    $self->shell({silent => 0}, "echo 'extension=" . $dst . lc($self->shortname()) . ".so' > /tmp/50-extension-" . $self->shortname() . ".ini");
+}
+
+1;
