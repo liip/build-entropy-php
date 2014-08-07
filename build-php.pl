843f81fa68e4f6382fbd383ddbe3969f77d6c71d
Christian Stocker <me@chregu.tv>
2014-08-07 08:01:35 +0200

    5.6.0RC3

--- build-php.pl
+++ build-php.pl
@@ -57,7 +57,7 @@ my $config = Config->new(
 			suffix       => '-apache2',
 		},
 	},
-	version              => '5.6.0RC2',
+	version              => '5.6.0RC3',
 	release              => 1,
 	debug                => 1,
 );
