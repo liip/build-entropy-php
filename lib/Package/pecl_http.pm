8cbf1c2439e7f2f1c3ccea354bebe4174c02301c
Christian Stocker <me@chregu.tv>
2014-11-02 09:43:09 +0100

    Added pecl_http support

---
+++ lib/Package/pecl_http.pm
@@ -0,0 +1,56 @@
+package Package::pecl_http;
+
+use strict;
+use warnings;
+
+use base qw(Package::peclbase);
+
+our $VERSION = '2.1.3';
+
+sub init {
+    my $self = shift;
+    $self->SUPER::init(@_);
+    $self->{PACKAGE_NAME} = 'pecl_http';
+    $self->{VERSION} = $VERSION;
+}
+
+sub packagesrcdir {
+    my $self = shift @_;
+    return $self->config()->srcdir() . "/" . $self->packagename() . "/";
+}
+
+sub configure_flags {
+        my $self = shift @_;
+        my (%args) = @_;
+        return join " ", (
+                $self->SUPER::configure_flags(@_),
+                '--with-http-libcurl-dir=' . $self->config()->prefix(),
+                '--with-http-libevent-dir=' . $self->config()->prefix(),
+                '--with-http-zlib-dir=' . $self->config()->prefix()
+        );
+}
+
+sub cc {
+        my $self = shift @_;
+        my $prefix = $self->config()->prefix();
+
+        # - the -L forces our custom iconv before the apple-supplied one
+        # - the -I makes sure the libxml2 version number for phpinfo() is picked up correctly,
+        #   i.e. ours and not the system-supplied libxml
+        return $self->SUPER::cc(@_) . " -I$prefix/include/php/ext/raphf  -I$prefix/include/php/ext/propro";
+}
+
+sub install {
+        my $self = shift @_;
+        return undef if ($self->is_installed());
+
+        $self->build();
+
+        my $dst = $self->install_prefix() . '/lib/php/extensions/no-debug-non-zts-20131226/';
+
+        $self->shell(sprintf("sudo cp modules/%s.so $dst", "http"));
+        $self->shell({silent => 0}, "echo 'extension=" . $dst . "http.so' > /tmp/60-extension-" . $self->shortname() . ".ini");
+    $self->shell("sudo mv /tmp/60-extension-" . $self->shortname() . ".ini " .$self->install_prefix() . "/php.d/")
+}
+
+return 1;
