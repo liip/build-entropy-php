4798275e7c167d4b6632428b18da5a93fcbf2fd1
Christian Stocker <me@chregu.tv>
2016-06-28 17:16:26 +0200

    use relative path for opcache.soy

--- lib/Package/php5.pm
+++ lib/Package/php5.pm
@@ -213,7 +213,7 @@ sub create_dso_ini_files {
 	# TODO: fixme
 	$self->shell({silent => 0}, "echo 'mssql.charset = UTF-8' >> $prefix/php.d/50-extension-mssql.ini");
 	my $dst = $self->full_install_extension_dir();
-	$self->shell({silent => 0}, "echo 'zend_extension=" . $dst . "opcache.so' > $prefix/php.d/20-extension-opcache.ini");
+	$self->shell({silent => 0}, "echo 'zend_extension=opcache.so' > $prefix/php.d/20-extension-opcache.ini");
     	# some default value
     	$self->shell({silent => 0}, "echo '[opcache]' >>  $prefix/php.d/20-extension-opcache.ini");
     	$self->shell({silent => 0}, "echo 'opcache.enable=0' >>  $prefix/php.d/20-extension-opcache.ini");
