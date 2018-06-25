021d6a340f1e898a46d1f890c5de625444757c7f
Christian Stocker <me@chregu.tv>
2018-06-25 09:29:41 +0200

    7.1.19

--- build-php.pl
+++ build-php.pl
@@ -60,7 +60,7 @@ my $config = Config->new(
 			suffix       => '-apache2',
 		},
 	},
-	version              => '7.1.18',
+	version              => '7.1.19',
 	release              => 1,
 	debug                => 0,
 );
