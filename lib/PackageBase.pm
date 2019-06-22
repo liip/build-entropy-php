9acfe0f58e7835812d45a5c1235cc3bca38b5c4a
Christian Stocker <me@chregu.tv>
2019-06-22 11:34:29 +0200

    use wget

--- lib/PackageBase.pm
+++ lib/PackageBase.pm
@@ -121,9 +121,9 @@ sub download {
 	$_->download() foreach $self->dependencies();
 	return if ($self->is_downloaded());
 	$self->log("downloading $self from " . $self->url());
-   #     $self->shell('wget', '-O', $self->download_path(), $self->url());
+        $self->shell('wget', '-O', $self->download_path(), $self->url());
 
-       $self->shell('/usr/local/Cellar/curl/7.38.0/bin/curl', '-L -o', $self->download_path(), $self->url());
+   #    $self->shell('/usr/local/Cellar/curl/7.38.0/bin/curl', '-L -o', $self->download_path(), $self->url());
 
 }
 
